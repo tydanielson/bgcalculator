3f775a44de6d6310eead42be9c2471d4d58f9a60
Tyler Danielson <tysdanielson@gmail.com>
2016-01-30 15:33:52 -0600

    update to app.js to add some values

--- app.js
+++ app.js
@@ -10,6 +10,23 @@ var app = angular.module('wsi', ['ngMaterial'])
 
 app.controller('WsiCtrl', ['$scope', function($scope){
 
+	$scope.ben = {
+		marketCap: 1,
+		currentAssets: 1,
+		currentDebt: 1,
+		longTermDebt: 1,
+		workingCapital: 1,
+		yrsPositiveIncome: 1,
+		yrsDividend: 1,
+		yrsGrowth: 1,
+		pe: 1,
+		pb: 1,
+		dividend: 1
+	};
+
+	$scope.calculate = function(){
+		console.log($scope.ben);
+	}
 
 }]);
 })();
