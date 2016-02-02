5eeb324d27fd9c204066c7fc8b7a401fa770d07c
Tyler Danielson <tysdanielson@gmail.com>
2016-02-01 19:38:17 -0600

    update to include call the last10k

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 (function(){
 
 var app = angular.module('wsi', ['ngMaterial'])
-    .config(function($mdThemingProvider) {
+    .config(function($mdThemingProvider, $httpProvider) {
         $mdThemingProvider.theme('default')
             .primaryPalette('green')
             .accentPalette('blue');
 
+		$httpProvider.defaults.headers.common = {};
+		$httpProvider.defaults.headers.post = {};
+		$httpProvider.defaults.headers.put = {};
+		$httpProvider.defaults.headers.patch = {};
+		$httpProvider.defaults.headers.common["Content-Type"] = "text/plain";
+ 		$httpProvider.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+ 		$httpProvider.defaults.headers.common['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS';
+		$httpProvider.defaults.headers.common['Ocp-Apim-Subscription-Key'] = '';
+		$httpProvider.defaults.useXDomain = true;
+		delete $httpProvider.defaults.headers.common['X-Requested-With'];
+
 });
 
-app.controller('WsiCtrl', ['$scope', function($scope){
+app.controller('WsiCtrl', function($scope, $http){
 
 	$scope.ben = {
 		marketCap: 1,
@@ -24,9 +35,41 @@ app.controller('WsiCtrl', ['$scope', function($scope){
 		dividend: 1
 	};
 
-	$scope.calculate = function(){
+	$scope.calculate = function() {
 		console.log($scope.ben);
 	}
 
-}]);
+	$scope.getTickerData = function() {
+		console.log($scope.ben.ticker);
+
+		var url = 'https://services.last10k.com/v1/company/DE/balancesheet?formType=10-K&filingOrder=0';
+		$http.get(url)			//callback: 'JSON_CALLBACK',
+		.then(function(data){
+			console.log(data);
+		}, function(data){
+			console.log(data);
+		})
+		var params = {
+            // Request parameters
+            "formType": "10-K",
+            "filingOrder": "0",
+        };
+      
+        // $.ajax({
+        //     url: "https://services.last10k.com/v1/company/DE/balancesheet&" + $.param(params),
+        //     beforeSend: function(xhrObj){
+        //         // Request headers
+        //         xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","5053da1874224fb1a0de3c4cd5f51a60");
+        //     },
+        //     type: "GET"
+        // })
+        // .done(function(data) {
+        //     alert("success");
+        // })
+        // .fail(function() {
+        //     alert("error");
+        // });
+	}
+
+});
 })();
