467bc79b38104b34949eeb6afc32a173a983f03f
Tyler Danielson <tysdanielson@gmail.com>
2016-02-03 16:27:49 -0600

    add lookups for balance, stock and ratio calls

--- app.js
+++ app.js
@@ -27,49 +27,66 @@ app.controller('WsiCtrl', function($scope, $http){
 		currentDebt: 1,
 		longTermDebt: 1,
 		workingCapital: 1,
-		yrsPositiveIncome: 1,
-		yrsDividend: 1,
-		yrsGrowth: 1,
+		yrsPositiveIncome: '',
+		yrsDividend: '',
+		yrsGrowth: '',
 		pe: 1,
 		pb: 1,
 		dividend: 1
 	};
 
+	$scope.ratios = {};
+
 	$scope.calculate = function() {
 		console.log($scope.ben);
-	}
+	};
 
-	$scope.getTickerData = function() {
-		console.log($scope.ben.ticker);
+	$scope.getCompanyData = function() {
+		//$scope.getBalanceSheet();
+		$scope.getStockQuote();
+		$scope.getRatios();
+	};
 
-		var url = 'https://services.last10k.com/v1/company/DE/balancesheet?formType=10-K&filingOrder=0';
-		$http.get(url)			//callback: 'JSON_CALLBACK',
-		.then(function(data){
-			console.log(data);
-		}, function(data){
-			console.log(data);
-		})
-		var params = {
-            // Request parameters
-            "formType": "10-K",
-            "filingOrder": "0",
-        };
-      
-        // $.ajax({
-        //     url: "https://services.last10k.com/v1/company/DE/balancesheet&" + $.param(params),
-        //     beforeSend: function(xhrObj){
-        //         // Request headers
-        //         xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","5053da1874224fb1a0de3c4cd5f51a60");
-        //     },
-        //     type: "GET"
-        // })
-        // .done(function(data) {
-        //     alert("success");
-        // })
-        // .fail(function() {
-        //     alert("error");
-        // });
-	}
+	$scope.getBalanceSheet = function() {
+		var url = 'https://services.last10k.com/v1/company/' + $scope.ben.ticker + '/balancesheet?formType=10-K&filingOrder=0';
+		$http.get(url)			
+			.then(function(data){
+				console.log("balance", data);
+				$scope.balance = data.data;
+			}, function(data){
+				alert('error');
+			});
+	};
+
+	$scope.getStockQuote = function() {
+		var url = 'https://services.last10k.com/v1/company/' + $scope.ben.ticker + '/quote'
+		$http.get(url)			
+			.then(function(data){
+				console.log("quote", data);
+				$scope.quote = data.data;
+			}, function(data){
+				alert('error');
+			});
+	};
+
+	$scope.getRatios = function() {
+		var url = 'https://services.last10k.com/v1/company/' + $scope.ben.ticker + '/ratios'
+		$http.get(url)			
+			.then(function(data){
+				console.log("ratios", data);
+				$scope.ratios.currentRatio = data.data.CurrentRatio.Recent["Latest Qtr"];
+				$scope.ratios.dividend = data.data.Dividends.Recent.TTM;
+				$scope.ratios.quick = data.data.QuickRatio.Recent["Latest Qtr"];
+				console.log(data.data.WorkingCapital.Historical);
+
+				var workingCapital = data.data.WorkingCapital.Historical[Object.keys(data.data.WorkingCapital.Historical)[Object.keys(data.data.WorkingCapital.Historical).length - 1]];
+				var longTermDebt = data.data.LongTermDebt.Recent["Latest Qtr"];
+				console.log(workingCapital, longTermDebt);
+				//$scope.ratios.longTermToWorking =
+			}, function(data){
+				alert('error');
+			});
+	};
 
 });
 })();
